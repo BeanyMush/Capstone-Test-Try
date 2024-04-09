f21f6ef3fed628c9f3c305aec25c5f55052fffa7
Beany Mush <125583675+BeanyMush@users.noreply.github.com>
2024-04-09 15:55:20 +0800

    Work on the user profile

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'creativetimofficial/material-dashboard',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c799330ecdef32b67277c64191ac3bac21089f5f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'creativetimofficial/material-dashboard' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c799330ecdef32b67277c64191ac3bac21089f5f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
